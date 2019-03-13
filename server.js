e6581a50a6edecb9c4e47c124ac20001e0a41dac
Doesntmeananything <andrey.krasnov.pm@gmail.com>
2019-03-13 15:26:51 +0300

    Prepared for deployment to heroku

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const helmet = require('helmet');
+const express = require("express");
+const mongoose = require("mongoose");
+const helmet = require("helmet");
+const path = require("path");
 // DB config
-const db = require('./config/keys').mongoURI;
+const db = require("./config/keys").mongoURI;
 
-const items = require('./routes/api/items');
+const items = require("./routes/api/items");
 
 const app = express();
 
@@ -15,17 +16,27 @@ app.use(express.urlencoded({ extended: true }));
 // Connect to DB
 try {
   mongoose.connect(db, { useNewUrlParser: true });
-  console.log('MongoDB connected');
+  console.log("MongoDB connected");
 } catch (error) {
   console.log(error);
 }
 
-app.get('/', (req, res) => {
-  res.send('Hello');
+app.get("/", (req, res) => {
+  res.send("Hello");
 });
 
 // Use routes
-app.use('/api/items', items);
+app.use("/api/items", items);
+
+// Server static assets if in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const port = process.env.PORT || 5000;
 app.listen(port);
